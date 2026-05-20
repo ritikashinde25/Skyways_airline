123498ca29d1be356e5c43a07c3279e706bdd3cb
Shreya Saini <shreya.saini@mphasis.com>
2026-05-20 10:47:27 +0530

    removed sensitive environment file from tracking

--- src/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  stripePublishableKey: 'pk_test_51TYOIXGSZQfOA0NVX3gZ7JHkClZTcqCe7i0hyq20ij3hcC08ePQvXnE3NtamrxDkoMNLpNvy6q03Pkcro33zNELV00Pfu0hNDJ'
-};
